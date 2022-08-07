f6a648ca16a7811d188aa978e8529d696a6322fb
Andrey <Plyakin.andrei@yandex.ru>
2022-08-07 12:39:41 +0400

    задача4

---
+++ Seminar1/a_task_4/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+int i = 1;
+
+while (i <= N);
+    if (i % 2 == 0)
+    {
+        Console.Write ($"{N} -> ");
+        Console.Write (i);
+        i = i + 2;
+    }
+    else
+    {
+        i = i + 1;
+    }
