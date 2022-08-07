f0b09e1af454fdf5ca0b443fc5ba7449dca05bb8
Andrey <Plyakin.andrei@yandex.ru>
2022-08-07 12:49:31 +0400

    задача4

--- Seminar1/a_task_4/Program.cs
+++ Seminar1/a_task_4/Program.cs
@@ -1,12 +1,11 @@
 ﻿Console.Write("Введите число: ");
 int N = int.Parse(Console.ReadLine());
 int i = 1;
-
+Console.Write ($"{N} -> ");
 while (i <= N)
     if (i % 2 == 0)
     {
-        Console.Write ($"{N} -> ");
-        Console.Write (i);
+        Console.Write ($"{i} ");
         i = i + 2;
     }
     else
