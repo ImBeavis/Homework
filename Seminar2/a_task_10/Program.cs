6ca8cae5c496b9219299b2b6c1836b64994b7089
Andrey <Plyakin.andrei@yandex.ru>
2022-08-09 23:18:46 +0400

    Задача10

---
+++ Seminar2/a_task_10/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+int a1 = number / 100;
+int a2 = number / 10;
+int a3 = number % 10;
+int result = a2 - (a1 * 10);
+Console.Write(result);
