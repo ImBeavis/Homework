f71b9e480fbb23daa6fc9e61393d5ac2747d95ea
Andrey <Plyakin.andrei@yandex.ru>
2022-08-10 00:01:52 +0400

    задача13

---
+++ Seminar2/a_task_13/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+while (number > 99)
+{
+    number = number % 10;
+    Console.Write(number);
+}
+Console.Write("Третьей цифры нет");
