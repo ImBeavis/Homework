1858aa34daa0564821acb7013cb7a05fe55a380b
Andrey <Plyakin.andrei@yandex.ru>
2022-08-10 00:10:16 +0400

    задача15

---
+++ Seminar2/a_task_15/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число обозначающую день недели: ");
+int number = int.Parse(Console.ReadLine());
+if (number == 1 || number == 2 || number == 3 || number == 4 || number == 5)
+{
+    Console.Write("Нет");
+}
+else 
+{
+    Console.Write("Да");
+}
