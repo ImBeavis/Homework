5a5798c6acd9b9ca64ad1331ef3a632053d54742
Andrey <Plyakin.andrei@yandex.ru>
2022-08-12 22:59:32 +0400

    задача23

---
+++ Seminar3/a_task_23/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+Console.Write($"{n} -> ");
+for (int i = 1; i <= n; i++)
+{
+    double num = Math.Pow(i, 3);
+    Console.Write($"{num} ");
+}
