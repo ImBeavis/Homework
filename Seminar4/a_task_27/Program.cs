6a1f245608d3a53a0fbdc3c6ef2acec53aad68a1
Andrey <Plyakin.andrei@yandex.ru>
2022-08-17 19:28:29 +0400

    задача27

---
+++ Seminar4/a_task_27/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+int Sum(int a)
+{
+    int result = 0;
+    while (a > 0)
+    {
+        result = a % 10 + result;
+        a /= 10; 
+    }
+    return result;
+}  
+Console.Write(Sum(num));
