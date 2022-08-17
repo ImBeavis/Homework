ebe8435fbf0dac497b073475089743aebf606cbf
Andrey <Plyakin.andrei@yandex.ru>
2022-08-17 20:24:44 +0400

    задача29

---
+++ Seminar4/a_task_29/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите размер массива: ");
+int num = int.Parse(Console.ReadLine());
+Console.Write("Введите диапазон чисел от: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите диапазон чисел до: ");
+int b = int.Parse(Console.ReadLine());
+
+Console.Write($"Массив размером {num} символов, заполненый числами в диапазоне от {a} до {b}: ");
+
+void PrintArray()
+{
+    int[] arr = new int[num];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(a, b);
+        Console.Write($"{arr[i]} ");
+    }
+}
+PrintArray();
