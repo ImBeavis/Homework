8a23bd9d21c24fe0ca2c9d4c0a956468e4081ec0
Andrey <Plyakin.andrei@yandex.ru>
2022-08-17 18:46:00 +0400

    задача25

---
+++ Seminar4/a_task_25/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число: ");
+int numA = int.Parse(Console.ReadLine());
+Console.Write("Введите степень возведения: ");
+int numB = int.Parse(Console.ReadLine());
+
+int Exponentiate(int a, int b)
+{
+    int sum = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        sum = sum * a;
+    }
+    return sum;
+}
+
+Console.Write($"{numA} в степени {numB} -> {Exponentiate(numA, numB)}");
